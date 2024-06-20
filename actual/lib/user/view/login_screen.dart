332295d25c53f2194a06eef3c677b5a178ae48f3
firstquarterlee <firstquarterlee@gmail.com>
2024-06-20 17:10:44 +0900

    06/20 공부 login, splash 구현

--- actual/lib/user/view/login_screen.dart
+++ actual/lib/user/view/login_screen.dart
@@ -1,16 +1,41 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:actual/common/component/custom_text_form_field.dart';
 import 'package:actual/common/const/colors.dart';
+import 'package:actual/common/const/data.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/common/view/root_tab.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = '';
+  String password = '';
+
+  @override
   Widget build(BuildContext context) {
+
+    final dio = Dio();
+
+    final emulatorIp = '10.0.2.2:3000'; //localhost
+    final simulatorIp = '127.0.0.1:3000';
+
+    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
     return DefaultLayout(
-      child: SingleChildScrollView(//키보드 올라오면 안짤리게하는거임, 스크롤이됨
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,//자동으로키보드뜸
+      child: SingleChildScrollView(
+        //키보드 올라오면 안짤리게하는거임, 스크롤이됨
+        keyboardDismissBehavior:
+            ScrollViewKeyboardDismissBehavior.onDrag, //자동으로키보드뜸
         child: SafeArea(
           top: true,
           bottom: false,
@@ -20,7 +45,9 @@ class LoginScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 _Title(),
-                const SizedBox(height: 16.0,), //사이 띄우는 용도
+                const SizedBox(
+                  height: 16.0,
+                ), //사이 띄우는 용도
                 _SubTitle(),
                 Image.asset(
                   'asset/img/misc/logo.png',
@@ -28,17 +55,56 @@ class LoginScreen extends StatelessWidget {
                 ),
                 CustomTextFormField(
                   hintText: '이메일을 입력해주세요.',
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    //onChanged는 입력한 값을 받아옴
+                    username = value; //온체인지에서 콜백으로 받은 값을 username에 계속 받는다.
+                  },
                 ),
-                const SizedBox(height: 16.0,), //사이 띄우는 용도
+                const SizedBox(
+                  height: 16.0,
+                ), //사이 띄우는 용도
                 CustomTextFormField(
                   hintText: '비밀번호를 입력해주세요.',
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    password = value;
+                  },
                   obscureText: true, //비번 블라킹
                 ),
-                const SizedBox(height: 16.0,), //사이 띄우는 용도
+                const SizedBox(
+                  height: 16.0,
+                ), //사이 띄우는 용도
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    // ID:비밀번호
+                    final rawString = '$username:$password';
+
+                    print(rawString);
+
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+                    String token = stringToBase64.encode(rawString);
+
+                    final resp = await dio.post(
+                      'http://$ip/auth/login',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Basic $token',
+                        },
+                      ),
+                    );
+
+                    final refreshToken = resp.data['refreshToken'];
+                    final accessToken = resp.data['accessToken'];
+                    
+                    await storage.write(key: REFRESH_TOKEN_KEY, value: refreshToken);
+                    await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+                    Navigator.of(context).push(//로그인 눌렀을때 roottap으로 이동함!!!
+                      MaterialPageRoute(
+                        builder: (_) => RootTab(),
+                      ),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: PRIMARY_COLOR,
                   ),
@@ -47,7 +113,21 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    final refreshToken =
+                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTcxODg2NTk2MywiZXhwIjoxNzE4OTUyMzYzfQ.tFVZxl1vVs3QViIVMyTv-nA6FKqKzZFHlRf5Dno7rY4';
+
+                    final resp = await dio.post(
+                      'http://$ip/auth/token',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Bearer $refreshToken',
+                        },
+                      ),
+                    );
+
+                    //print(resp.data);
+                  },
                   style: TextButton.styleFrom(
                     foregroundColor: Colors.black,
                   ),
